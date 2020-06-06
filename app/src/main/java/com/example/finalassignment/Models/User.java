15f84d43c603e0e991ea6b5eac9d72c432c6131e
yugesh <ahltoea@gmail.com>
2020-06-06 16:31:47 +0545

    final

--- app/src/main/java/com/example/finalassignment/Models/User.java
+++ app/src/main/java/com/example/finalassignment/Models/User.java
@@ -9,7 +9,7 @@ import androidx.room.PrimaryKey;
 import java.io.Serializable;
 
 /**
- * Created by Zoom on 10/1/2018.
+ * Created by Yugesh sapkota on 6/1/2020.
  */
 @Entity
 public class User implements Serializable {
