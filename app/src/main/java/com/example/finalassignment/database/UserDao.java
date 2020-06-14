93a14b0329260bbb216b0fc4922f065092bf1343
yugesh <ahltoea@gmail.com>
2020-06-14 17:04:33 +0545

    todo app

--- app/src/main/java/com/example/finalassignment/database/UserDao.java
+++ app/src/main/java/com/example/finalassignment/database/UserDao.java
@@ -6,6 +6,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Transaction;
 import androidx.room.Update;
 
 import com.example.finalassignment.Models.User;
