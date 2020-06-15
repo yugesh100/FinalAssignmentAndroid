a8b0dfb940e56cd2ef9bb849f7b38c65c40b5fc0
yugesh <ahltoea@gmail.com>
2020-06-15 12:44:15 +0545

    one to many relation

--- app/src/main/java/com/example/finalassignment/database/DateConverter.java
+++ app/src/main/java/com/example/finalassignment/database/DateConverter.java
@@ -5,13 +5,13 @@ import androidx.room.TypeConverter;
 import java.util.Date;
 
 public class DateConverter {
-    @TypeConverter
-    public static Date toDate(Long timeStamp){
-        return timeStamp == null ? null : new Date(timeStamp);
-    }
-
-    @TypeConverter
-    public static Long toTimeStamp(Date date){
-        return date == null ? null : date.getTime();
-    }
+//    @TypeConverter
+//    public static Date toDate(Long timeStamp){
+//        return timeStamp == null ? null : new Date(timeStamp);
+//    }
+//
+//    @TypeConverter
+//    public static Long toTimeStamp(Date date){
+//        return date == null ? null : date.getTime();
+//    }
 }
